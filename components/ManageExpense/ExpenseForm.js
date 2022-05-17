0b3917dbf0427e26109253daa5ff36abedf8a21b
Schadrack <chadrackngirimana@gmail.com>
2022-05-17 18:48:01 +0200

    styling inputs

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,12 +1,61 @@
-import { View } from "react-native"
-
+import { StyleSheet, Text, View } from "react-native";
+import Input from "./Input";
 
 const ExpenseForm = () => {
+  const amountChangedHandler = () => {};
   return (
-      <View>
-          
-    </View>
-  )
-}
+      <View style={styles.form}>
+          <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          style={styles.inputRow}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onchangeText: amountChangedHandler,
+          }}
+        />
+        <Input
+          style={styles.inputRow}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
 
-export default ExpenseForm
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          // autoCapitalize:'sentences',
+          //   autoCorrect:false/default is true
+          onChangeText: () => {},
+        }}
+      />
+    </View>
+  );
+};
+
+export default ExpenseForm;
+const styles = StyleSheet.create({
+    form: {
+        marginTop:40
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        marginVertical: 24,
+        textAlign:'center'
+    },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  inputRow: {
+    flex: 1,
+  },
+});
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,6 @@
 import { useContext, useLayoutEffect } from "react";
 import { StyleSheet, TextInput, View } from "react-native";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
@@ -45,8 +46,8 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
+      <ExpenseForm />
       <View style={styles.buttons}>
-        <TextInput/>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>
           Cancel
         </Button>
