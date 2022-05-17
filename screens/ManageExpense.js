a1d5088b2bab9ce3e09acb09e19914f84aa5e6d1
Schadrack <chadrackngirimana@gmail.com>
2022-05-17 11:59:05 +0200

    custom buttons

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,8 +1,12 @@
-import { useLayoutEffect } from 'react';
-import { View ,Text} from 'react-native';
+import { useLayoutEffect } from "react";
+import { StyleSheet, View } from "react-native";
 
-const ManageExpense = ({route,navigation}) => {
-  const editedExpenseId = route?.params?.expenseId;
+import Button from "../components/UI/Button";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
+
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
   useLayoutEffect(() => {
@@ -10,11 +14,59 @@ const ManageExpense = ({route,navigation}) => {
       title: isEditing ? "Edit Expense" : "Add Expense",
     });
   }, [navigation, isEditing]);
- 
-    return (<View>
-      <Text>Manage Expenses</Text>
+
+  function deleteExpenseHandler() {}
+
+  function cancelHandler() {}
+
+  function confirmHandler() {}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
       </View>
-  )
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
-export default ManageExpense
+export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
